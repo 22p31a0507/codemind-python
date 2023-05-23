b2efddb2eb3c75b91d1edfba2ce491c327b1b22b
22p31a0507 <119730122+22p31a0507@users.noreply.github.com>
2023-05-23 23:06:17 +0530

    t=int(input()) for i in range(t):     n=int(input())     a=list(map(int,input().split()))     b=sorted(a)     if a==b:         print("0")     else:         print(max(a)-min(a))

--- Optimal_Sorting.py
+++ Optimal_Sorting.py
@@ -1,10 +1,9 @@
-a=int(input())
-while(a):
-    b=int(input())
-    s=list(map(int,input().split()))
-    b=sorted(s)
-    if(s==b):
-        print(0)
+t=int(input())
+for i in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    b=sorted(a)
+    if a==b:
+        print("0")
     else:
-        print(max(b)-min(b))
-    a-=1
+        print(max(a)-min(a))
